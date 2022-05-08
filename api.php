efe6de4b4d201c0327a52e89476c058801219d01
AnaKonotop <nasity48@gmail.com>
2022-05-08 16:56:24 +0300

    fending form data tp api

--- api.php
+++ api.php
@@ -2,23 +2,33 @@
 header('Content-type: application/json');
 require_once('config.php');
 
-// $data = json_decode( file_get_contents('php://input'), true );
+$data = json_decode( file_get_contents('php://input'), true );
 
 /* TEST action profile_add, list_users & profiles_list */
+/* 
 $data['action'] = 'profile_add';
+$data['action'] = 'specializations_add';
 $data['action'] = 'list_users';
 $data['action'] = 'profiles_list';
-$data['name'] = 'Petro';
+// $data['action'] = 'job_titles_list';
+// $data['action'] = 'specializations_list';
+// $data['action'] = 'profiles_by_job_title';
+// $data['action'] = 'profiles_by_specialization';
+// $data['action'] = 'profile_get';
+// $data['action'] = 'profile_edit';
+$data['name'] = 'Stepan1';
+$data['id'] = 1;
 $data['middlename'] = 'Mikhailovich';
 $data['surname'] = 'Petrenko';
 $data['img'] = 'img-profiles/temp.png';
-$data['job_title'] = 'CEO';
-$data['specialization'] = 'Pharmacology';
-/* END TEST */
+$data['job_title'] = 1;
+$data['specialization'] = 1; */
+
 
 if ($data['action'])
 {
     switch ($data['action']) {
+        /* Auth uUsers */
         case 'validate_user':
             $login = $data['user'];
             $password = $data['password'];
@@ -36,25 +46,80 @@ if ($data['action'])
             $login = $data['user'];
             remove_user($login);
             break;
+        /* Profiles */
         case 'profile_add':
             $name = $data['name'];
             $middlename = $data['middlename'];
             $surname = $data['surname'];
             $img = $data['img'];
-            $job_title = $data['job_title'];
-            $specialization = $data['specialization'];
-            profile_add($name, $middlename, $surname, $img, $job_title, $specialization);
+            // $job_title_id = $data['job_title'];
+            // $specialization_id = $data['specialization'];
+            $job_title_id = 1;
+            $specialization_id = 1;
+            var_dump($data['img']);
+            profile_add($name, $middlename, $surname, $img, $job_title_id, $specialization_id);
             break;
         case 'profiles_list':
             profiles_list();
             break;
+        case 'profile_get':
+            $id = $data['id'];
+            profile_get($id);
+            break;
         case 'profile_edit':
             $id = $data['id'];
-            profile_edit($id);
+            $name = $data['name'];
+            $middlename = $data['middlename'];
+            $surname = $data['surname'];
+            $img = $data['img'];
+            $job_title_id = $data['job_title'];
+            $specialization_id = $data['specialization'];
+            profile_edit($id, $name, $middlename, $surname, $img, $job_title_id, $specialization_id);
             break;
         case 'profile_delete':
             $id = $data['id'];
             profile_delete($id);
+            break;
+        /* Job titles */
+        case 'job_titles_add':
+            $name = $data['name'];
+            job_titles_add($name);
+            break;
+        case 'job_titles_list':
+            job_titles_list();
+            break;
+        case 'job_titles_edit':
+            $id = $data['id'];
+            job_titles_edit($id);
+            break;
+        case 'job_titles_delete':
+            $id = $data['id'];
+            job_titles_delete($id);
+            break;
+        /* Specializations */
+        case 'specializations_add':
+            $name = $data['name'];
+            specializations_add($name);
+            break;
+        case 'specializations_list':
+            specializations_list();
+            break;
+        case 'specializations_edit':
+            $id = $data['id'];
+            specializations_edit($id);
+            break;
+        case 'specializations_delete':
+            $id = $data['id'];
+            specializations_delete($id);
+            break;
+        /* Profiles sort */
+        case 'profiles_by_job_title':
+            $id = $data['id'];
+            profiles_by_job_title($id);
+            break;
+        case 'profiles_by_specialization':
+            $id = $data['id'];
+            profiles_by_specialization($id);
             break;
         default:
             echo 'Nothing to see here';
@@ -93,8 +158,9 @@ function list_users(){
     global $db;
     $stmt = $db->prepare("SELECT id, user, creation_date FROM `auth-users`");
     $stmt->execute();
-    $profiles = $stmt->fetch(PDO::FETCH_ASSOC);
-    echo json_encode($profiles);
+    $list = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    if ($list !=true ) echo 'No users ;-/';
+    else echo json_encode($list);
 }
 function remove_user($login){
     global $db;
@@ -106,26 +172,56 @@ function remove_user($login){
     else echo 'User removed';
 }
 /* Profiles */
-function profile_add($name, $middlename, $surname, $img, $job_title, $specialization){
+function profile_add($name, $middlename, $surname, $img, $job_title_id, $specialization_id){
     global $db;
-    $query = "INSERT INTO `profiles` (name, middlename, surname, img, job_title, specialization, date_added) 
+    $query = "INSERT INTO `profiles` (name, middlename, surname, img, job_title_id, specialization_id, date_added) 
               VALUES (?, ?, ?, ?, ?, ?, NOW())";
     $insert = $db->prepare($query);
+    print_r($img);
     $insert->execute(array(
-        $name, $middlename, $surname, $img, $job_title, $surname
+        $name, $middlename, $surname, $img, $job_title_id, $specialization_id
     ));
-    echo 'user added';
+    echo 'profile added';
 
 }
 function profiles_list(){
     global $db;
-    $stmt = $db->prepare("SELECT * FROM `profiles`");
+    $stmt = $db->prepare(
+        "SELECT profiles.id, profiles.name, middlename, surname, img, date_added, 
+        index_job_titles.name as job_title_name, 
+        index_specializations.name as specialization_name 
+        FROM `profiles` 
+        LEFT JOIN `index_job_titles` ON profiles.job_title_id = index_job_titles.id 
+        LEFT JOIN `index_specializations` ON profiles.specialization_id = index_specializations.id"
+    );
     $stmt->execute();
-    $profiles = $stmt->fetch(PDO::FETCH_ASSOC);
-    echo json_encode($profiles);
+    $list = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    if ($list !=true ) echo 'No profiles ;-/';
+    else echo json_encode($list);
 }
-function profile_edit($id){
-    
+function profile_get($id){
+    global $db;
+    $stmt = $db->prepare(
+        "SELECT profiles.id, profiles.name, middlename, surname, img, date_added, 
+        index_job_titles.name as job_title_name, 
+        index_specializations.name as specialization_name 
+        FROM `profiles` 
+        LEFT JOIN `index_job_titles` ON profiles.job_title_id = index_job_titles.id 
+        LEFT JOIN `index_specializations` ON profiles.specialization_id = index_specializations.id
+        WHERE profiles.id=?"
+    );
+    $stmt->execute([$id]);
+    $list = $stmt->fetch(PDO::FETCH_ASSOC);
+    if ($list !=true ) echo 'No profiles ;-/';
+    else echo json_encode($list);
+}
+function profile_edit($id, $name, $middlename, $surname, $img, $job_title_id, $specialization_id){
+    global $db;
+    $stmt = "UPDATE `profiles` SET name=?, middlename=?, surname=?, img=?, job_title_id=?, specialization_id=? WHERE id=?";
+    $stmt = $db->prepare($stmt); 
+    $stmt->execute([$name, $middlename, $surname, $img,  $job_title_id, $specialization_id, $id]);
+    if ($stmt->rowCount()) echo "Updated profile id #$id";
+    else echo "No profile updated";
 }
 function profile_delete($id){
     global $db;
@@ -136,5 +232,100 @@ function profile_delete($id){
     if( ! $stmt->rowCount() ) echo "Error: Deletion failed";
     else echo 'Profile removed';
 }
+/* Job titles */
+function job_titles_add($name){
+    global $db;
+    $query = "INSERT INTO `index_job_titles` (name) VALUES (?)";
+    $insert = $db->prepare($query);
+    $insert->execute([$name]);
+    echo 'job title added';
 
+}
+function job_titles_list(){
+    global $db;
+    $stmt = $db->prepare("SELECT * FROM `index_job_titles`");
+    $stmt->execute();
+    $list = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    if ($list !=true ) echo 'No job titles ;-/';
+    else echo json_encode($list);
+}
+function job_titles_edit($id){
+    
+}
+function job_titles_delete($id){
+    global $db;
+    $query = "DELETE FROM `index_job_titles` WHERE  id=:id";
+    $stmt = $db->prepare($query);
+    $stmt->bindParam(':id', $id);
+    $stmt->execute();
+    if( ! $stmt->rowCount() ) echo "Error: Deletion failed";
+    else echo 'job title removed';
+}
 
+/* Specializations */
+function specializations_add($name){
+    global $db;
+    $query = "INSERT INTO `index_specializations` (name) VALUES (?)";
+    $insert = $db->prepare($query);
+    $insert->execute([$name]);
+    echo 'specialization added';
+
+}
+function specializations_list(){
+    global $db;
+    $stmt = $db->prepare("SELECT * FROM `index_specializations`");
+    $stmt->execute();
+    $list = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    if ($list !=true ) echo 'No specializations ;-/';
+    else echo json_encode($list);
+}
+function specializations_edit($id){
+    
+}
+function specializations_delete($id){
+    global $db;
+    $query = "DELETE FROM `index_specializations` WHERE  id=:id";
+    $stmt = $db->prepare($query);
+    $stmt->bindParam(':id', $id);
+    $stmt->execute();
+    if( ! $stmt->rowCount() ) echo "Error: Deletion failed";
+    else echo 'specialization removed';
+}
+/* Profiles sort */
+function profiles_by_job_title($id){
+    global $db;
+    $stmt = $db->prepare(
+        "SELECT profiles.id, profiles.name, middlename, surname, img, date_added, 
+        index_job_titles.name as job_title_name, 
+        index_specializations.name as specialization_name 
+        FROM `profiles`
+        LEFT JOIN `index_job_titles` ON profiles.job_title_id = index_job_titles.id 
+        LEFT JOIN `index_specializations` ON profiles.specialization_id = index_specializations.id
+        WHERE profiles.job_title_id = :id"
+    );
+    $stmt->bindParam(':id', $id);
+    $stmt->execute();
+    $list = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    if ($list !=true ) echo 'No profiles ;-/';
+    else echo json_encode($list);
+}
+function profiles_by_specialization($id){
+    global $db;
+    $stmt = $db->prepare(
+        "SELECT profiles.id, profiles.name, middlename, surname, img, date_added, 
+        index_job_titles.name as job_title_name, 
+        index_specializations.name as specialization_name 
+        FROM `profiles`
+        LEFT JOIN `index_job_titles` ON profiles.job_title_id = index_job_titles.id 
+        LEFT JOIN `index_specializations` ON profiles.specialization_id = index_specializations.id
+        WHERE profiles.specialization_id = :id"
+    );
+    $stmt->bindParam(':id', $id);
+    $stmt->execute();
+    $list = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    if ($list !=true ) echo 'No profiles ;-/';
+    else echo json_encode($list);
+}
+/* Profiles sort */
+function sort_by_job_titles(){}
+function sort_by_specializations(){}
--- profile-create.js
+++ profile-create.js
@@ -2,15 +2,80 @@ const reg_form = document.getElementById('reg_form');
 const reg_name = document.getElementById('reg_name');
 const reg_surname = document.getElementById('reg_surname');
 const reg_middlename = document.getElementById('reg_middlename');
+const job_title = document.getElementById('reg_job_title');
+const job_specialization = document.getElementById('reg_specialization');
 const reg_file = document.getElementById('reg_file');
 
 reg_form.addEventListener('submit', e => {
   e.preventDefault();
 
-  // signInUser(login.value, password.value);
+  // console.log(reg_name.value);
+  // console.log(reg_surname.value);
+  // console.log(reg_middlename.value);
+  // console.log(job_title.value);
+  // console.log(job_specialization.value);
 
-  console.log(reg_name.value);
-  console.log(reg_surname.value);
-  console.log(reg_middlename.value);
-  console.log(reg_file.files[0]);
+  const [img] = reg_file.files;
+
+  // const data = {
+  //   action: "profile_add",
+  //   name: reg_name.value,
+  //   surname: reg_surname.value,
+  //   middlename: reg_middlename.value,
+  //   job_title: job_title.value,
+  //   job_specialization: job_specialization.value,
+  // }
+
+  const formData = new FormData();
+
+  formData.append('action', 'profile_add');
+  formData.append('name', reg_name.value);
+  formData.append('surname', reg_surname.value);
+  formData.append('middlename', reg_middlename.value);
+  formData.append('job_title', job_title.value);
+  formData.append('job_specialization', job_specialization.value);
+  formData.append('img', img);
+
+  fetch('./api.php', {
+    method: 'POST',
+    // headers: { 'Content-Type': "multipart/form-data" },
+    // body: JSON.stringify(data),
+    body: formData,
+  });
 });
+
+const getSelectData = async (action = 'job_titles_list') => {
+
+  const data = { action }
+
+  const data_2 = await fetch('./api.php', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(data),
+  });
+  return data_2;
+}
+
+getSelectData('job_titles_list')
+  .then(res => res.json())
+  .then((data) => {
+    // console.log('YES!', data);
+    data.forEach(item => {
+      const option = document.createElement('option');
+      option.textContent = item.name;
+      option.value = item.id;
+      job_title.append(option);
+    })
+  });
+
+getSelectData('specializations_list')
+  .then(res => res.json())
+  .then((data) => {
+    // console.log('YES!', data);
+    data.forEach(item => {
+      const option = document.createElement('option');
+      option.textContent = item.name;
+      option.value = item.id;
+      job_specialization.append(option);
+    })
+  });
